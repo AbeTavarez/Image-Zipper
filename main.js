0649ef1c1bc8c9f9b7ea4cd5d64fa7102832f4b6
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-02-13 00:00:40 -0500

    main window created

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('running script')
+const {app, BrowserWindow} = require('electron')
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: 'Image Zipper',
+        width: 500,
+        height:600
+    })
+}
+
+app.on('ready', createMainWindow)
