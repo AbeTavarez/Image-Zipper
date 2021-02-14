8507fbb3b041c50b1f099418b9e1ccc575efaed1
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-02-13 23:48:15 -0500

    file loaded

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ function createMainWindow(){
         title: 'Image Zipper',
         width: 500,
         height:600,
-        icon: `./assets/icons/Icon_256x256.png`
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`
     })
 
-    // LOAD URL/WEBSITE OR FILE
+    // ***** LOAD URL/WEBSITE OR FILE
     // mainWindow.loadURL(`http://www.abrahamtavarez.dev`)
     // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
-    // YOU CAN ALSO LOAD FILE USING THIS LOAD FUNCTION
+    // ***** YOU CAN ALSO LOAD FILE USING THIS LOAD FUNCTION
     mainWindow.loadFile(`./app/index.html`)
 }
 
