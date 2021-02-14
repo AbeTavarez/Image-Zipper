ebd8cf4344c6ae2857cac973d23882b24ee5d8bc
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-02-13 23:31:49 -0500

    app dir created

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const {app, BrowserWindow} = require('electron')
+let mainWindow
 
 function createMainWindow(){
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'Image Zipper',
         width: 500,
         height:600
     })
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 }
 
 app.on('ready', createMainWindow)
